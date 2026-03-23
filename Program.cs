85c318ef400e5887c42220ad0a021380432b5d37
Meu Nome <joelma.carmo.duarte@gmail.com>
2026-03-23 00:38:02 -0300

    Exercício Cidadesxxx

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\nSegundo a Wikipédia as cidades do Litoral Sul são:\n");
+Console.WriteLine("• Iguape");
+Console.WriteLine("• Ilha comprida");
+Console.WriteLine("• Cananeia\n");
+
+Console.WriteLine("As cidades da Baixada Santista são:\n");
+Console.WriteLine("• Bertioga");
+Console.WriteLine("• Guarujá");
+Console.WriteLine("• Santos");
+Console.WriteLine("• São Vicente");
+Console.WriteLine("• Praia Grande");
+Console.WriteLine("• Mongaguá");
+Console.WriteLine("• Itanhaém");
+Console.WriteLine("• Peruíbe\n");
+
+Console.WriteLine("As cidades do Litoral Norte são:\n");
+Console.WriteLine("• Ubatuba");
+Console.WriteLine("• Caraguatatuba");
+Console.WriteLine("• Ilhabela");
+Console.WriteLine("• São Sebastião\n");
+
+Console.WriteLine("Fontes de Pesquisas:");
+Console.WriteLine("https://pt.wikipedia.org/wiki/Litoral_de_São_Paulo");
+Console.WriteLine("https://pt.wikipedia.org/wiki/Litoral_de_São_Paulo#/media/Ficheiro:LitoralPaulista.jpg\n");
